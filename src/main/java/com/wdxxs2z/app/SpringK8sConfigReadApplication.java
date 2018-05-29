30666bbcf4649c20144bca6d58bdb62e345873a2
wdxxs2z <243219650@qq.com>
2018-05-29 15:21:36 +0800

    first commit.

---
+++ src/main/java/com/wdxxs2z/app/SpringK8sConfigReadApplication.java
@@ -0,0 +1,12 @@
+package com.wdxxs2z.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringK8sConfigReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringK8sConfigReadApplication.class, args);
+	}
+}
